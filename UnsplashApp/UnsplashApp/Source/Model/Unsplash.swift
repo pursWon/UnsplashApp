130cc4b66b1c8cd5839fe3d0736acc69c2f36cfb
WONGI HONG <dt95@naver.com>
2023-04-17 12:17:01 +0900

    MOD : Model 형태 알맞게 수정

--- UnsplashApp/UnsplashApp/Source/Model/Unsplash.swift
+++ UnsplashApp/UnsplashApp/Source/Model/Unsplash.swift
@@ -1,17 +1,10 @@
 import Foundation
 
-struct PhotoData: Decodable {
-    let data: [Photos]
-}
 
 struct Photos: Decodable {
-    let id: String
     let urls: Size
 }
 
 struct Size: Decodable {
     let small: String
 }
-
-
-
